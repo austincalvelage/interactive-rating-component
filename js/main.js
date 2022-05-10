5c0950ec2159b9faa28fc38371edb441bdd6512f
Austin Calvelage <austin.calvelage@icloud.com>
2022-05-10 02:42:57 -0400

    Built out submit Handler allowing component to switch from ratingState to ratedState UI

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ function rateHandler(event) {
   event.target.classList.add('selected');
 }
 
+function submitHandler(event) {
+  userRating === undefined ?  alert('Choose a rating!') : ratingState.classList.add('hidden'), ratedState.classList.remove('hidden'), displayRating.textContent = userRa;
+}
 
 rating.forEach(rate => rate.addEventListener('click', rateHandler));
 submitBtn.addEventListener('click', submitHandler);
